305e28a3ec875d009d401dc64b9db7487afb700a
Emiliano-DLC <emiliano_r_d_l_c@outlook.com>
2020-02-21 22:30:34 -0700

    2/21/2020- Worked on findTailExhaustive()

--- Week5-6Lab/src/SnakeGame.java
+++ Week5-6Lab/src/SnakeGame.java
@@ -7,17 +7,33 @@ public class SnakeGame {
     public SnakeGame() {
         this.game = new boolean[0][0];
     }
-
-    public SnakeGame(boolean[][] arr, int x, int y) {
-        this.game  = new boolean[arr.length][arr[0].length];
-        for(int i = 0; i<arr.length; i++){
-            for(int j = 0; j<arr[i].length;j++){
-                this.game[i][j] = arr[i][j];
+    public SnakeGame(boolean[][] game, int x, int y) {
+        this.game  = new boolean[game.length-1][game[0].length-1];
+        for(int i = 0; i < game.length; i++) {
+            for (int j = 0; j < game[i].length; j++) {
+                this.game[i][j] = game[i][j];
             }
         }
+        this.headPosition = new int[2];
+        this.headPosition[0]=x;
+        this.headPosition[1]=y;
     }
 
+    //Return x,y,counterChecks
     public int[] findTailExhaustive() {
-        return;
+        int[] countTail = new int[3];
+        countTail[0] = headPosition[0];
+        countTail[1]= headPosition[1];
+        int counterChecks = 0;
+        for(int i = 0; i < game.length; i++){
+            for(int k = 0; k < game[i].length; k++){
+                counterChecks++;
+                if(game[i][k] == game[headPosition[0]][headPosition[1]]){
+                    countTail[2]=counterChecks;
+                    return countTail;
+                }
+            }
+        }
+        return countTail;
     }
 }
