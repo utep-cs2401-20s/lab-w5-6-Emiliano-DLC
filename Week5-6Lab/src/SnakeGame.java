8aa23c310ba71997342b2c26692eced16e0d6e12
Emiliano-DLC <emiliano_r_d_l_c@outlook.com>
2020-02-26 13:18:51 -0700

    2/26/2020 NEED RECURSIVE

--- Week5-6Lab/src/SnakeGame.java
+++ Week5-6Lab/src/SnakeGame.java
@@ -2,11 +2,12 @@ public class SnakeGame {
     private boolean[][] game;
     private int[] headPosition;
     private static int exhaustiveCheck;
-    private int recursiveCheck;
+    private static int recursiveCheck;
 
     public SnakeGame() {
         this.game = new boolean[0][0];
     }
+
     public SnakeGame(boolean[][] game, int x, int y) {
         this.game  = new boolean[game.length-1][game[0].length-1];
         for(int i = 0; i < game.length; i++) {
@@ -24,16 +25,36 @@ public class SnakeGame {
         int[] countTail = new int[3];
         countTail[0] = headPosition[0];
         countTail[1]= headPosition[1];
-        int counterChecks = 0;
+        exhaustiveCheck = 0;
         for(int i = 0; i < game.length; i++){
             for(int k = 0; k < game[i].length; k++){
-                counterChecks++;
+                exhaustiveCheck++;
                 if(game[i][k] == game[headPosition[0]][headPosition[1]]){
-                    countTail[2]=counterChecks;
+                    countTail[2] = exhaustiveCheck;
                     return countTail;
                 }
             }
         }
         return countTail;
+    }
+
+    /*public int[] findTailRecursive(){
+    }
+
+    private int[] findTailRecursive(int currentPosition, int[] previousPosition){
+    }*/
+
+    private void resetCounters(){
+        exhaustiveCheck = 0;
+        recursiveCheck = 0;
+
+    }
+
+    private static int getRecursiveChecks(){
+        return recursiveCheck;
+    }
+
+    private static int getExhaustiveCheck(){
+        return exhaustiveCheck;
     }
 }
